00c064fa05b88af443e13d1d3df6c318f195ac2e
sudhirka7 <121374488+sudhirka7@users.noreply.github.com>
2024-01-20 13:17:50 +0530

    Add files via upload
    index commit

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Youth community</title>
+    <link rel="stylesheet" href="style.css"> 
+    <link rel="stylesheet" href="../css/res.css">
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Catamaran:wght@200&family=Courgette&family=Dancing+Script:wght@700&family=Edu+TAS+Beginner:wght@700&family=Lato:wght@300;900&family=Mukta:wght@700&family=Mulish:wght@300&family=Open+Sans&family=PT+Sans:ital,wght@1,700&family=Poppins:wght@300&family=Raleway:wght@100&family=Roboto&family=Roboto+Condensed:wght@700&family=Roboto+Slab&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/f30fac2c61.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <div class="logo">
+                <img src="../images/H.jpg" alt="hello">
+            </div>
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="#Gallery">GALLERY</a></li>
+                <li><a href="#coordinator">COORDINATOR</a></li>
+                    <li class="dropdown">
+                    <button class="dropbtn">LOGIN</button>
+                    <ul class="dropdown-content">
+                        <li><a href="login_formadmin.php">Admin</a></li>
+                        <li><a href="login_formuser.php">User</a></li>
+                        <li><a href="createCommunity.php">Create community</a></li>
+                    </ul>   
+                </li>
+                 
+                <li><a href="#blog">BLOG</a></li>
+                <li><a href="#review">REVIEWS</a></li>
+                <i id="bar" class="fa-solid fa-bars"></i>
+
+            </ul>
+        </nav>
+        <div class="main"> 
+            <div class="mainText">
+            
+                <h1>Discover the Best community & <br> make is Fresh Community</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>
+                 Perferendis incidunt officiis, vitae corporis ipsum optio <br> architecto maxime doloremque facere dicta!                </p>
+                <button>Explore sudhirs</button>
+            </div>
+            
+            <img src="../images/community.jpg" alt="community">
+            
+        </div>
+
+        <div class="div"><img src="../images/youthlogo.gif" alt=""></div>
+
+        <div id="Gallery">
+            <div class="head">
+                <h1>Our Members</h1>
+            </div>
+            <div class="comm">
+                <div class="card">
+                    <img src="../images/1.JPG" alt="">
+                    <p>Rooster Chicken Rice Bowl</p>
+                    <button class="cart">Add To Cart</button>
+                </div>
+                <div class="card">
+                    <img src="../images/5.JPG" alt="">
+                    <p>Rooster Chicken Rice Bow</p>
+                    <button class="cart">Add To Cart</button>
+                </div>
+                <div class="card">
+                    <img src="../images/6.JPG" alt="">
+                    <p>Rooster Chicken Rice Bow</p>
+                    <button class="cart">Add To Cart</button>
+                </div>
+                <div class="card">
+                    <img src="../images/4.JPG" alt="">
+                    <p>Rooster Chicken Rice Bow</p>
+                    <button class="cart">Add To Cart</button>
+                </div>
+                <div class="card">
+                    <img src="../images/3.JPG" alt="">
+                    <p>Rooster Chicken Rice Bow</p>
+                    <button class="cart">Add To Cart</button>
+                </div>
+                <div class="card">
+                    <img src="../images/2.JPG" alt="">
+                    <p>Rooster Chicken Rice Bow</p>
+                    <button class="cart">Add To Cart</button>
+                </div>
+            </div>
+        </div>
+
+ 
+        <div id="review">
+            <div class="head">
+                <h1>User Review's</h1>
+            </div>
+            <div class="reviews">
+                <div class="reviewCard">
+                    <img src="../images/sudhir.PNG" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus modi reprehenderit culpa possimus!</p>
+                </div>
+                <div class="reviewCard">
+                    <img src="../images/sudhir1.JPG" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia sequi nesciunt odio cupiditate omnis.</p>
+                </div>
+                <div class="reviewCard">
+                    <img src="../images/sudhir2.JPG" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima consequatur aspernatur magni autem facere.</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="coordinator">
+            <div class="head">
+                <h1>Coordinators</h1>
+            </div>
+            <div class="trendcomm">
+                <div class="trends">
+                    <img src="../images/god.JPEG" alt="">
+                    <img src="../images/god1.jpg" alt="">
+                    <img src="../images/god2.jpg" alt="">
+
+                </div>
+                <div class="trends">
+                    <img src="blg4.JPG" alt="">
+                    <img src="blg5.jpg" alt="">
+                    <img src="blg6.jpg" alt="">
+
+                </div>
+                <div class="trends">
+                    <img src="sudhir1.JPG" alt="">
+                    <img src="blg4.JPG" alt="">
+                    <img src="sudhir3.JPG" alt="">
+
+                </div>
+            </div>
+        </div>
+
+        <div id="blog">
+            <div class="head">
+                <h1>Read Community Blogs</h1>
+            </div>
+            <div class="blogs">
+                <div class="blogCard">
+                    <img src="blg2.JPG" alt="">
+                    <p>Latest sudhir Blogs</p>
+                    <a href="https://iamasudhirblog.com/">Read Blog</a>
+                </div>
+                <div class="blogCard">
+                    <img src="blg1.jpg" alt="">
+                    <p>Latest sudhir Blogs</p>
+                    <a href="https://iamasudhirblog.com/">Read Blog</a>
+                </div>
+                <div class="blogCard">
+                    <img src="blg5.jpg" alt="">
+                    <p>Latest sudhir Blogs</p>
+                    <a href="https://iamasudhirblog.com/">Read Blog</a>
+                </div>
+            </div>
+        </div>
+
+        <div id="Events">
+            <div class="head">
+                <h1>Upcoming Events</h1>
+            </div>
+            <div class="blogs">
+                <div class="blogCard">
+                    <img src="blg2.JPG" alt="">
+                    <p>Latest Community Blogs</p>
+                    <a href="#">Read Blog</a>
+                </div>
+                <div class="blogCard">
+                    <img src="blg1.jpg" alt="">
+                    <p>Latest Community Blogs</p>
+                    <a href="#">Read Blog</a>
+                </div>
+                <div class="blogCard">
+                    <img src="blg5.jpg" alt="">
+                    <p>Latest Community Blogs</p>
+                    <a href="#">Read Blog</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer">
+            <div class="text">
+                <h3>About Us</h3>
+                <p>Lorem ipsum dolor sit amet consectetur <br>
+                 adipisicing elit. Odit consequuntur par <br>
+                  velit accusamus, animi ullam harum dolores <br> libero cum suscipit. Molestiae, commodi.</p>
+  
+            </div>
+            <div class="text">
+                <h3>Our Community</h3>
+                <p>Quality</p>
+                <p>Affordable</p>
+                <p>Best Exeprience</p>
+                <p>Low Effort</p>
+
+            </div>
+            <div class="text">
+                <h3>Offers</h3>
+                <p>Make community</p>
+                <p>See information</p>
+                <p>Easiest</p>
+                <p></p>
+
+            </div>
+            <div class="text">
+                <h3>Connect Us</h3>
+                <p>Linked In</p>
+                <p>Facebook</p>
+                <p>Instagram</p>
+                <p>Twitter</p>
+
+            </div>
+        </div>
+    </div>
+
+
+
+       <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+ 
+    <script src="index.js"></script>
+</body>
+</html> 
